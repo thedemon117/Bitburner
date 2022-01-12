dea5ef2136b3ff75daa0e7ed5f856392eda65c0f
demon117 <demon.werx@gmail.com>
2022-01-11 20:44:11 -0700

    smarter haxxoring

---
+++ ns2-scripts/smart-test.js
@@ -0,0 +1,45 @@
+import {sendIT} from '/remote.js';
+import {maxThreads} from "/sInfo.js";
+
+/** @param {NS} ns **/
+export async function main(ns) {
+    let target = ns.args[0];
+    let serv = ns.args[1];
+
+    let grow_time = ns.getGrowTime(target);
+    let weaken_time = ns.getWeakenTime(target);
+    let hack_time = ns.getHackTime(target);
+
+
+    let minSecLevel = ns.getServerMinSecurityLevel(target)    
+    let curSecLevel = ns.getServerSecurityLevel(target);
+
+    await sendIT(ns, serv);
+
+    // fire up a weaken script
+    ns.run("weaken.js", 500, target); // this should be run from "home"
+    ns.run("grow.js", 1000, target); // this should be run from "home"
+
+    while (true) {
+        let maxMonies = ns.getServerMaxMoney(target);
+        let availMonies = ns.getServerMoneyAvailable(target);
+        
+        let hThreads = maxThreads(ns, serv, "sim-hack.js");
+        let gThreads = maxThreads(ns, serv, "sim-grow.js");
+
+        if (availMonies >= (maxMonies * .9)) {
+            ns.killall(serv);
+            ns.exec("sim-hack.js", serv, hThreads, target); // this should be a personal server to throw hack threads at the target
+        } else {
+            ns.killall(serv);
+            ns.exec("sim-grow.js", serv, gThreads, target); // this should be a personal server to throw hack threads at the target
+            await ns.sleep(grow_time + 1000);
+            // run grow from home, with sufficent threads determined by a 2x or 4x calc?
+            // need to write and scp with sendIT a simple grow script
+            //let this loop till money is to a certain point, then hack
+        }
+       
+    }
+
+
+}
