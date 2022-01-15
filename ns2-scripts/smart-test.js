a96b1ec50d13efdcfb14ffad53fb3854f2d1e873
demon117 <demon.werx@gmail.com>
2022-01-15 09:30:14 -0700

    fixed script handler

--- ns2-scripts/smart-test.js
+++ ns2-scripts/smart-test.js
@@ -1,8 +1,10 @@
 import {sendIT} from '/remote.js';
 import {maxThreads} from "/sInfo.js";
 
+
 /** @param {NS} ns **/
 export async function main(ns) {
+    
     let target = ns.args[0];
     let serv = ns.args[1];
 
@@ -17,7 +19,7 @@ export async function main(ns) {
     await sendIT(ns, serv);
 
     // fire up a weaken script
-    ns.run("weaken.js", 500, target); // this should be run from "home"
+    ns.run("weaken.js", 1000, target); // this should be run from "home"
     ns.run("grow.js", 1000, target); // this should be run from "home"
 
     while (true) {
@@ -26,15 +28,23 @@ export async function main(ns) {
         
         let hThreads = maxThreads(ns, serv, "sim-hack.js");
         let gThreads = maxThreads(ns, serv, "sim-grow.js");
+        let wThreads = maxThreads(ns, serv, "sim-weaken.js");
 
-        if (availMonies >= (maxMonies * .9)) {
-            ns.killall(serv);
+        ns.print((availMonies >= (maxMonies * .8))+" and "+((minSecLevel + 5) >= curSecLevel));
+        ns.print("Must be ~ True True ~ to hack");
+        ns.print("");
+        if (availMonies >= (maxMonies * .8) && ((minSecLevel + 2) >= curSecLevel)) {
+            ns.print("Weaken Time: "+weaken_time);
+            ns.run("sim-weaken.js", 1000, target);
+            //ns.killall(serv);
             ns.exec("sim-hack.js", serv, hThreads, target); // this should be a personal server to throw hack threads at the target
-            await ns.sleep(grow_time + 1000);
+            await ns.sleep(10000);
         } else {
-            ns.killall(serv);
+            ns.print("Weaken Time: "+weaken_time);
+            //ns.killall(serv);
+            ns.run("sim-weaken.js", 1000, target, 1);
             ns.exec("sim-grow.js", serv, gThreads, target); // this should be a personal server to throw hack threads at the target
-            await ns.sleep(grow_time + 1000);
+            await ns.sleep(30000);
             // run grow from home, with sufficent threads determined by a 2x or 4x calc?
             // need to write and scp with sendIT a simple grow script
             //let this loop till money is to a certain point, then hack
